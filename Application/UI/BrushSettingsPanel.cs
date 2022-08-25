12f04d4d08490f6c54cbde7c0b867a9325cae618
Jarrod Doyle <thejarroddoyle@gmail.com>
2022-08-25 14:52:04 +0100

    Add brush settings to the panel

--- Application/UI/BrushSettingsPanel.cs
+++ Application/UI/BrushSettingsPanel.cs
@@ -1,4 +1,5 @@
 using ImGuiNET;
+using Simulation;
 
 namespace Application.UI;
 
@@ -21,6 +22,27 @@ public class BrushSettingsPanel : Panel
 
         if (ImGui.Begin("Brush Settings", ref open))
         {
+            {
+                string[] items = Enum.GetNames(typeof(Simulation.ElementType));
+                var current = (int) BrushManager.Element;
+                if (ImGui.Combo("Element", ref current, items, items.Length))
+                    BrushManager.Element = (Simulation.ElementType)current;
+            }
+            
+            {
+                string[] items = Enum.GetNames(typeof(BrushShape));
+                var current = (int) BrushManager.BrushShape;
+                if (ImGui.Combo("Shape", ref current, items, items.Length))
+                    BrushManager.BrushShape = (BrushShape) current;
+            }
+
+            {
+                // TODO: Try working out how to use InputScalar here
+                var size = (int) BrushManager.BrushSize;
+                if (ImGui.InputInt("Size", ref size))
+                    BrushManager.BrushSize = (uint) Math.Clamp(size, 1, 20);
+            }
+
             ImGui.End();
         }
 
